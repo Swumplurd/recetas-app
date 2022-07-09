6a647690dd5203135d6000e7696c73dadf100114
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-07-09 16:11:42 -0500

    deje la aplicacion lista para empezar

---
+++ src/components/RecetasApp.jsx
@@ -0,0 +1,12 @@
+export const RecetasApp = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1>RecetasApp</h1>
+            </div>
+            
+        </div>
+    </div>
+  )
+};
