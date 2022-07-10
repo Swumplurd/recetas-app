172faba80f0429d05f7bdba0fd0ed1ecda5caf2e
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-07-10 17:50:28 -0500

    falta tema de colores

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,18 +2,44 @@ import React from "react";
 
 export const LoginPage = () => {
   return (
-    <div class="card text-center">
-      <div class="card-header">Featured</div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-5">
+          <div className="card">
+            <div className="card-header text-center card-login-header">Login</div>
+            <div className="card-body card-login-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Nombre de Usuario
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control input-body"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Contraseña
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div className="d-flex justify-content-center">
+                  <button type="submit" className="btn btn-recetas">
+                    Inicia sesión
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="card-footer text-muted">2 days ago</div>
     </div>
   );
 };
